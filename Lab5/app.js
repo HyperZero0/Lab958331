c6707daf280081a17d1353fc80c9747320b39b75
HyperZero0 <140488668+HyperZero0@users.noreply.github.com>
2024-07-31 09:25:54 +0000

    Update Week5

---
+++ Lab5/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+    app.get('/Assignment2', function(req, res){
+        const q = req.query
+    res.send("Name: Krergunchai Cunatarakul <br/> Login Date: " + q.date + "" + q.month + "" + q.year)
+    })
+app.listen(9999,function(){
+console.log("Server is running on PORT 9999")
+})
